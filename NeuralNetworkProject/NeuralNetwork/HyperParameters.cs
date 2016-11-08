85b70de8c88a3ee5ee26a1080a0e9d91eed2db76
Obada <obj1995@hotmail.com>
2016-11-09 00:47:45 +0200

    Enable some configs like learning rates and maxEpochs

---
+++ NeuralNetworkProject/NeuralNetwork/HyperParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkProject.NeuralNetwork
+{
+    public class HyperParameters
+    {
+        public double[] LearningRates { get; set; }
+        public int maxEpochs { get; set; }
+        public double maxError { get; set; }
+    }
+}
--- NeuralNetworkProject/Training/BackpropagationAlgorithm.cs
+++ NeuralNetworkProject/Training/BackpropagationAlgorithm.cs
@@ -10,9 +10,53 @@ namespace NeuralNetworkProject.Training
 {
     public class BackpropagationAlgorithm : ITrainingAlgorithm
     {
-        public Tuple<IList<Matrix<double>>, IList<double>, IList<double>> Train(NeuralNetwork.NeuralNetwork neuralNetwork, Matrix<double> trainingSet, Matrix<double> crossValidationSet, Matrix<double> trainingSetOutput, Matrix<double> crossValidationSetOutput)
+        public Tuple<IList<Matrix<double>>, IList<double>, IList<double>> Train(NeuralNetwork.NeuralNetwork neuralNetwork, Matrix<double> trainingSet, Matrix<double> crossValidationSet, Matrix<double> trainingSetOutput, Matrix<double> crossValidationSetOutput, HyperParameters hyperParameters = null)
+        {
+            double[] learningRates;
+            double maxError = 0.01, error = 5;
+            int maxEpochs = 1000, epochs = 0;
+            if (hyperParameters == null)
+            {
+                learningRates = new double[neuralNetwork.HiddenWeights.Count];
+                for (int i = 0; i < learningRates.Length; i++)
+                {
+                    learningRates[i] = 0.01;
+                }
+            }
+            else
+            {
+                if (hyperParameters.LearningRates.Length != neuralNetwork.HiddenWeights.Count)
+                    throw new ArgumentException("Learning rate number should equal hidden weights number");
+                if(maxEpochs <= 0)
+                    throw new ArgumentException("Max Epochs cannot be negative");
+                if(maxError > 2 || maxError < 0)
+                    throw new ArgumentException("Max error cannot be negative or very large");
+                learningRates = hyperParameters.LearningRates;
+                maxError = hyperParameters.maxError;
+                maxEpochs = hyperParameters.maxEpochs;
+            }
+            while(error > maxError || epochs++ <= maxEpochs)
+            {
+                for (int i = 0; i < trainingSet.RowCount; i++)
+                {
+                    Vector<double> input  = trainingSet.Row(i),
+                                   output = trainingSetOutput.Row(i);
+                    IList<Vector<double>> activations = neuralNetwork.ForwardInput(input);
+                    error = computeError(activations[activations.Count - 1], output);
+
+                }
+            }
+            return null;
+        }
+
+        protected virtual Matrix<double> computeNextWeights()
         {
             return null;
+        }
+
+        protected virtual double computeError(Vector<double> outputs, Vector<double> realOutputs)
+        {
+            return (outputs - realOutputs).Select(element => element * element).Sum();
         }
     }
 }
