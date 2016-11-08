e0b0f28a011068cf17b9cdd1522d330de60461d7
Obada <obj1995@hotmail.com>
2016-11-08 18:56:15 +0200

    Add NeuralNetwork class

---
+++ NeuralNetworkProject/NeuralNetwork/NeuralNetwork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkProject.NeuralNetwork
+{
+    public class NeuralNetwork
+    {
+        public ICollection<Layer> Layers { get; }
+    }
+
+    public class Layer
+    {
+        public int NeuronNumber { get; set; }
+    }
+}
