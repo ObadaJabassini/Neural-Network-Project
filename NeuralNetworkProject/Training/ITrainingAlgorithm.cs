c887e557608575eb0e736cfeeeaa075d09377435
Obada <obj1995@hotmail.com>
2016-11-09 00:46:29 +0200

    Add HyperParameter to Train method to enable some configs

--- NeuralNetworkProject/Training/ITrainingAlgorithm.cs
+++ NeuralNetworkProject/Training/ITrainingAlgorithm.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NeuralNetworkProject.NeuralNetwork;
 
 namespace NeuralNetworkProject.Training
 {
     public interface ITrainingAlgorithm
     {
-        Tuple<IList<Matrix<double>>, IList<double>, IList<double>> Train(NeuralNetwork.NeuralNetwork neuralNetwork, Matrix<double> trainingSet, Matrix<double> crossValidationSet, Matrix<double> trainingSetOutput, Matrix<double> crossValidationSetOutput);
+        Tuple<IList<Matrix<double>>, IList<double>, IList<double>> Train(NeuralNetwork.NeuralNetwork neuralNetwork, Matrix<double> trainingSet, Matrix<double> crossValidationSet, Matrix<double> trainingSetOutput, Matrix<double> crossValidationSetOutput, HyperParameters hyperParamters = null);
     }
 }
