bb8309afa764d725ae828abbe50cb9b8ab20f1a0
Obada <obj1995@hotmail.com>
2016-11-10 16:19:51 +0200

    Add a new creation function to create divider from a string

--- NeuralNetworkProject/DataAdapter/Dividers.cs
+++ NeuralNetworkProject/DataAdapter/Dividers.cs
@@ -12,12 +12,19 @@ namespace NeuralNetworkProject.DataAdapter
         public enum DividerType { STATIC}
         public static IDataDivider CreateDivider(DividerType dividerType)
         {
-            switch(dividerType)
+            switch (dividerType)
             {
                 case DividerType.STATIC:
                 default:
                     return new StaticDivider();
             }
+        }
+
+        public static IDataDivider CreateDivider(string dividerTypeName)
+        {
+            var modifiedName = dividerTypeName.ToLower();
+            modifiedName = Char.ToUpper(modifiedName[0]) + modifiedName.Substring(1);
+            return Activator.CreateInstance(Type.GetType($"NeuralNetworkProject.DataAdapter.{modifiedName}Divider")) as IDataDivider;
         }
     }
 }
