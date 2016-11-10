c839e39889b605645b63899fc8124e938c73d5b3
Obada <obj1995@hotmail.com>
2016-11-10 16:15:39 +0200

    Add a new Divider type by index

---
+++ NeuralNetworkProject/DataAdapter/IndexDivider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace NeuralNetworkProject.DataAdapter
+{
+    public class IndexDivider : IDataDivider
+    {
+        public Tuple<Matrix<double>, Matrix<double>, Matrix<double>, Matrix<double>> Divide(Matrix<double> inputs, Matrix<double> outputs, params object[] paramters)
+        {
+            IList<int> trainingIndices = (IList<int>) paramters[0],
+                       crossIndices    = (IList<int>) paramters[1];
+            return new Tuple<Matrix<double>, Matrix<double>, Matrix<double>, Matrix<double>>(Matrix<double>.Build.DenseOfRows(inputs.EnumerateRowsIndexed().Where(t => trainingIndices.Contains(t.Item1)).Select(t => t.Item2)),
+                                                                                             Matrix<double>.Build.DenseOfRows(outputs.EnumerateRowsIndexed().Where(t => trainingIndices.Contains(t.Item1)).Select(t => t.Item2)),
+                                                                                             Matrix<double>.Build.DenseOfRows(inputs.EnumerateRowsIndexed().Where(t => crossIndices.Contains(t.Item1)).Select(t => t.Item2)),
+                                                                                             Matrix<double>.Build.DenseOfRows(outputs.EnumerateRowsIndexed().Where(t => crossIndices.Contains(t.Item1)).Select(t => t.Item2)));
+        }
+    }
+}
