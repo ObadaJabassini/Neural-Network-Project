7165f882c4124143dac0cf982089e89feb0229eb
Obada <obj1995@hotmail.com>
2016-11-10 16:16:11 +0200

    Create a new Factory for dividers type

---
+++ NeuralNetworkProject/DataAdapter/Dividers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkProject.DataAdapter
+{
+    public class Dividers
+    {
+        private Dividers() { }
+        public enum DividerType { STATIC}
+        public static IDataDivider CreateDivider(DividerType dividerType)
+        {
+            switch(dividerType)
+            {
+                case DividerType.STATIC:
+                default:
+                    return new StaticDivider();
+            }
+        }
+    }
+}
