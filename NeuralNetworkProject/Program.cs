6bad213c81c35a68a9c45a79b9bfc5da023d9595
Obada Jabassini <obj1995@hotmail.com>
2016-11-22 17:21:08 +0200

    Update Program.cs

--- NeuralNetworkProject/Program.cs
+++ NeuralNetworkProject/Program.cs
@@ -1,11 +1,10 @@
-﻿using NeuralNetworkProject.DataAdapter;
+using MathNet.Numerics.LinearAlgebra;
+using NeuralNetworkProject.DataAdapter;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MathNet.Numerics.LinearAlgebra;
-using NeuralNetworkProject.Training;
 
 namespace NeuralNetworkProject
 {
@@ -13,18 +12,24 @@ namespace NeuralNetworkProject
     {
         public static void Main(string[] args)
         {
-            var t = DataReader.DataReader.Instance.ReadFromFile(@"C:\Users\DELL\Desktop\Datasets\test.txt");
-            Trainer trainer = new Training.Trainer() { };
-            trainer.TrainingAlgorithm = new BackPropagationWithMomentum();
-            NeuralNetwork.NeuralNetwork nn = new NeuralNetwork.NeuralNetworkBuilder().
-                                                              Layer(4, new Math.FunctionApplier()).
-                                                              Layer(2, new Math.FunctionApplier()).
-                                                              Layer(3, new Math.FunctionApplier()).
-                                                              Build();
-            trainer.Train(nn, t.Item1, t.Item2);
+            var t = DataReader.DataReader.Instance.ReadFromFile("D:/test.txt");
+            Training.Trainer trainer = new Training.Trainer();
+            //IDataDivider divider = Dividers.CreateDivider(Dividers.DividerType.INDEXED);
+            //var temp = divider.Divide(t.Item1, t.Item2, Enumerable.Range(0, t.Item1.RowCount * 6 / 10), Enumerable.Range(t.Item1.RowCount * 6 / 10 + 1, t.Item1.RowCount - 1));
+            //IDataDivider divider = Dividers.CreateDivider(Dividers.DividerType.RANDOMIZED);
+            //var temp = divider.Divide(t.Item1, t.Item2, 0.6);
+            //Console.WriteLine(temp.Item1);
+            //Console.WriteLine(temp.Item3);
+            //NeuralNetwork.NeuralNetwork nn = new NeuralNetwork.NeuralNetworkBuilder().
+            //                                                  Layer(4, new Math.FunctionApplier()).
+            //                                                  Layer(5, new Math.FunctionApplier()).
+            //                                                  Layer(3, new Math.FunctionApplier()).
+            //                                                  Build();
+            //trainer.Train(nn, t.Item1, t.Item2);
             // test case (should belong to the second class = [0 1 0])
-            var tt = nn.ForwardInput(Vector<double>.Build.DenseOfArray(new double[] { 5.5, 2.5, 4.0, 1.3 }));
-            Console.WriteLine(tt.Item1[tt.Item1.Count - 1]);
+            //var tt = nn.ForwardInput(Vector<double>.Build.DenseOfArray(new double[] { 5.5, 2.5, 4.0, 1.3 }));
+            //Console.WriteLine(tt.Item1[tt.Item1.Count - 1]);
+
             Console.ReadLine();
         }
     }
