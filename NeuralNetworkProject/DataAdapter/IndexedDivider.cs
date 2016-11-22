e23278ca8ce78cdb8c41871cadec01d3c2b0df71
Obada Jabassini <obj1995@hotmail.com>
2016-11-22 17:17:49 +0200

    Update IndexedDivider.cs

--- NeuralNetworkProject/DataAdapter/IndexedDivider.cs
+++ NeuralNetworkProject/DataAdapter/IndexedDivider.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,8 +13,16 @@ namespace NeuralNetworkProject.DataAdapter
 
         public Tuple<Matrix<double>, Matrix<double>, Matrix<double>, Matrix<double>> Divide(Matrix<double> inputs, Matrix<double> outputs, params object[] paramters)
         {
-            IList<int> trainingIndices = paramters[0] as IList<int>,
-                       crossIndices    = paramters[1] as IList<int>;
+            IEnumerable<int> trainingIndices = paramters[0] as IEnumerable<int>,
+                             crossIndices    = paramters[1] as IEnumerable<int>;
+            if (trainingIndices == null)
+            {
+                trainingIndices = Enumerable.Range(0, inputs.RowCount / 2);
+            }
+            if (crossIndices == null)
+            {
+                crossIndices = Enumerable.Range(inputs.RowCount / 2 + 1, inputs.RowCount - 1);
+            }
             return new Tuple<Matrix<double>, Matrix<double>, Matrix<double>, Matrix<double>>(Matrix<double>.Build.DenseOfRows(inputs.EnumerateRowsIndexed().Where(t => trainingIndices.Contains(t.Item1)).Select(t => t.Item2)),
                                                                                              Matrix<double>.Build.DenseOfRows(outputs.EnumerateRowsIndexed().Where(t => trainingIndices.Contains(t.Item1)).Select(t => t.Item2)).Transpose(),
                                                                                              Matrix<double>.Build.DenseOfRows(inputs.EnumerateRowsIndexed().Where(t => crossIndices.Contains(t.Item1)).Select(t => t.Item2)),
