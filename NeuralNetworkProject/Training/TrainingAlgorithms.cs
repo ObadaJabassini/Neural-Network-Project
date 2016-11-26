f7ddeca8ebdbe4551d43c1cfd38a2de1b47f643b
Obada <obj1995@hotmail.com>
2016-11-26 15:21:18 +0200

    Add a new factory class for creating min algorithms

---
+++ NeuralNetworkProject/Training/TrainingAlgorithms.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkProject.Training
+{
+    class TrainingAlgorithms
+    {
+        public enum TrainingMethod { GD, GDM, LM}
+        private TrainingAlgorithms() { }
+        public static TrainingAlgorithm Use(TrainingMethod method)
+        {
+            switch (method)
+            {
+                case TrainingMethod.GD:
+                default:
+                    return new BackpropagationAlgorithm();
+                case TrainingMethod.GDM:
+                    return new BackPropagationWithMomentumAlgorithm();
+                case TrainingMethod.LM:
+                    return new LevenbergAlgorithm();
+            }
+        }
+
+        public static TrainingAlgorithm Use(string method)
+        {
+            var modifiedName = method.ToLower();
+            modifiedName = Char.ToUpper(modifiedName[0]) + modifiedName.Substring(1);
+            return Activator.CreateInstance(Type.GetType($"NeuralNetworkProject.Training.{modifiedName}Algorithm")) as TrainingAlgorithm;
+        }
+    }
+}
