dd2581e47d0b9950dbd1a600a81dbcfc2f85f48a
Obada Jabassini <obj1995@hotmail.com>
2016-12-22 17:58:34 +0200

    Update LevenbergAlgorithm.cs

--- NeuralNetworkProject/Training/LevenbergAlgorithm.cs
+++ NeuralNetworkProject/Training/LevenbergAlgorithm.cs
@@ -31,42 +31,88 @@ namespace NeuralNetworkProject.Training
             TrainingErrorMessage message = new TrainingErrorMessage() { NeuralNetwork = neuralNetwork, TrainingSet = trainingSet, CrossValidationSet = crossValidationSet, TrainingSetOutput = trainingSetOutput, CrossValidationSetOutput = crossValidationSetOutput };
             while (error >= maxError && ++epochs <= maxEpochs)
             {
+                //var baseMatrix = trainingSet.EnumerateRows().Zip(trainingSetOutput.EnumerateColumns(), (first, second) => new Tuple<Vector<double>, Vector<double>>(first, second)).Select(sample =>
+                //{
+                //    Vector<double> input = sample.Item1,
+                //                   output = sample.Item2;
+                //    var temp = neuralNetwork.ForwardInput(input);
+                //    IList<Vector<double>> acs = temp.Item1, gs = temp.Item2;
+                //    IList<double> localErrors = new List<double>();
+                //    var D = (output - acs[acs.Count - 1]).PointwiseMultiply(gs[gs.Count - 1]).ToColumnMatrix();
+                //    var deltaW = D * acs[acs.Count - 2].ToRowMatrix();
+                //    IList<double> ers = new List<double>();
+                //    for (int i = deltaW.RowCount - 1; i >= 0; --i)
+                //    {
+                //        for (int k = deltaW.ColumnCount - 1; k >= 0; --k)
+                //        {
+                //            ers.Add(-deltaW[i, k]);
+                //        }
+                //    }
+                //    for (int k = layers.Count - 2; k > 0; k--)
+                //    {
+                //        D = (weights[k].Transpose() * D).RemoveRow(0).PointwiseMultiply(gs[k - 1].ToColumnMatrix());
+                //        deltaW = D * acs[k - 1].ToRowMatrix();
+                //        for (int i = deltaW.RowCount - 1; i >= 0; --i)
+                //        {
+                //            for (int v = deltaW.ColumnCount - 1; v >= 0; --v)
+                //            {
+                //                ers.Add(-deltaW[i, v]);
+                //            }
+                //        }
+                //    }
+                //    var t = output - acs[acs.Count - 1];
+                //    return new Tuple<Vector<double>, double>(Vector<double>.Build.DenseOfEnumerable(ers), t.PointwiseMultiply(t).Sum() / 2);
+                //});
                 var baseMatrix = trainingSet.EnumerateRows().Zip(trainingSetOutput.EnumerateColumns(), (first, second) => new Tuple<Vector<double>, Vector<double>>(first, second)).Select(sample =>
                 {
                     Vector<double> input = sample.Item1,
                                    output = sample.Item2;
                     var temp = neuralNetwork.ForwardInput(input);
                     IList<Vector<double>> acs = temp.Item1, gs = temp.Item2;
-                    IList<double> localErrors = new List<double>();
-                    var D = (output - acs[acs.Count - 1]).PointwiseMultiply(gs[gs.Count - 1]).ToColumnMatrix();
-                    var deltaW = D * acs[acs.Count - 2].ToRowMatrix();
-                    IList<double> ers = new List<double>();
-                    for (int i = deltaW.RowCount - 1; i >= 0; --i)
+                    IList<Vector<double>> local = new List<Vector<double>>();
+                    for (int pp = 0; pp < layers[layers.Count - 1].NeuronsNumber; pp++)
                     {
-                        for (int k = deltaW.ColumnCount - 1; k >= 0; --k)
+                        double delta = gs[gs.Count - 1][pp];
+                        IList<Vector<double>> list = new List<Vector<double>>();
+                        Vector<double> vec = delta * weights[weights.Count - 1].Row(pp) * gs[gs.Count - 2][pp];
+                        list.Add(vec);
+                        for (int mm = weights.Count - 2; mm >= 0; --mm)
                         {
-                            ers.Add(-deltaW[i, k]);
+                            vec = (weights[mm].Transpose() * vec).PointwiseMultiply(gs[mm]);
+                            list.Add(vec);
                         }
-                    }
-                    for (int k = layers.Count - 2; k > 0; k--)
-                    {
-                        D = (weights[k].Transpose() * D).RemoveRow(0).PointwiseMultiply(gs[k - 1].ToColumnMatrix());
-                        deltaW = D * acs[k - 1].ToRowMatrix();
-                        for (int i = deltaW.RowCount - 1; i >= 0; --i)
+                        list = list.Reverse().ToList();
+                        IList<double> ers = new List<double>();
+                        for (int i = list.Count - 1; i >= 0; i--)
                         {
-                            for (int v = deltaW.ColumnCount - 1; v >= 0; --v)
-                            {
-                                ers.Add(-deltaW[i, v]);
-                            }
+                            var tt = list[i].PointwiseMultiply(acs[i]);
+                            tt.Reverse().ToList().ForEach(ers.Add);
                         }
+                        local.Add(Vector<double>.Build.DenseOfEnumerable(ers));
                     }
                     var t = output - acs[acs.Count - 1];
-                    return new Tuple<Vector<double>, double>(Vector<double>.Build.DenseOfEnumerable(ers), t.PointwiseMultiply(t).Sum() / 2);
+                    return new Tuple<Matrix<double>, Vector<double>>(Matrix<double>.Build.DenseOfRowVectors(local), t.PointwiseMultiply(t) / 2);
                 });
                 message.Epochs = epochs;
-                var j = Matrix<double>.Build.DenseOfRows(baseMatrix.Select(element => element.Item1));
+                int index = 0;
+                var j = Matrix<double>.Build.Dense(trainingSet.RowCount * layers[layers.Count - 1].NeuronsNumber, layers[layers.Count - 1].NeuronsNumber);
+                foreach(var mat in baseMatrix.Select(element => element.Item1))
+                {
+                    for (int i = 0; i < mat.RowCount; i++)
+                    {
+                        j.SetRow(index++, mat.Row(i));
+                    }
+                }
                 var jTranspose = j.Transpose();
-                var errors = Vector<double>.Build.DenseOfEnumerable(baseMatrix.Select(element => element.Item2));
+                var errors = Vector<double>.Build.Dense(baseMatrix.Select(element => element.Item2.Count).Sum());
+                index = 0;
+                foreach(var vec in baseMatrix.Select(element => element.Item2))
+                {
+                    for (int i = 0; i < vec.Count; i++)
+                    {
+                        vec[index++] = vec[i];
+                    }
+                }
                 var prevWeights = neuralNetwork.HiddenWeights.Select(v => Matrix<double>.Build.DenseOfMatrix(v)).ToList();
                 var currentError = 50000.0;
                 var temp2 = jTranspose * j;
